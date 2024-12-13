9ff872795b86d4d9a6b499306684c82fafef03da
BIRKARAN-SINGH <132205922+BIRKARAN-SINGH@users.noreply.github.com>
2024-12-13 06:40:25 -0800

    Add files via upload

--- assignment#4_comp-360/arm.gd
+++ assignment#4_comp-360/arm.gd
@@ -1,107 +1,97 @@
 extends CharacterBody3D
 
-
-const SPEED = 5.0
+@export_group("Movement")
+@export var moveSpeed = 8.0
+@export var acceleration = 7.5
+var moveDir: Vector3
 const JUMP_VELOCITY = 5.5
-const LERP_VAL = .15
-var held_object = null
 
+@export var jumpForce = 5.5
+@export var gravity = 9.8
 
+@export_group("Camera")
+@export var mouseSens = Vector2(0.2, 0.2)
+@onready var camera = $Camera3D
 
-@onready var armature=$Armature
-@onready var pivot: Node3D = $camOrigin
-@onready var anim_tree = $AnimationTree
-@onready var animation_player = $AnimationPlayer
-@onready var camera = $camOrigin/SpringArm3D/Camera3D
-
-@export_category("Holding Objects")
+@export_group("Holding Objects")
 @export var throwForce = 7.5
-@export var followSpead = 5.0
+@export var followSpeed = 5.0
 @export var followDistance = 2.5
-@export var maxDistanceFromCanere = 5.0
-@export var dropBelomPLayer = false
+@export var maxDistanceFromCamera = 5.0
+@export var dropBelowPlayer = false
+@export var groundRay: RayCast3D # Only needed if dropBelowPlayer is true
 
-@onready var InteractRay=$camOrigin/SpringArm3D/Camera3D/InteractRay
+@onready var interactRay = $Camera3D/IntereactRay
 var heldObject: RigidBody3D
 
-@export var sens=0.5
-
-
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-var is_grabbing: bool = false
-
 func _ready():
-	Input.mouse_mode=Input.MOUSE_MODE_CAPTURED
-
-
-func _input(event):
-	if event is InputEventMouseMotion:
-		rotate_y(deg_to_rad(-event.relative.x*sens))
-		pivot.rotate_x(deg_to_rad(-event.relative.y*sens))
-		pivot.rotation.x=clamp(pivot.rotation.x, deg_to_rad(-90), deg_to_rad(45))
-	if Input.is_action_pressed("grab"):
-		if not is_grabbing:
-			animation_player.play("grab")
-			is_grabbing = true
-
-	elif Input.is_action_pressed("letgo"):
-		if is_grabbing:
-			animation_player.play("letgo")
-			is_grabbing = false
-func _physics_process(delta: float) -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	
+func _process(delta):
+	# Move Input
+	var inputDir = Input.get_vector("left", "right", "up", "down")
+	moveDir = (transform.basis * Vector3(inputDir.x, 0, inputDir.y)).normalized()
+	
+	# Jumping
+	if Input.is_action_just_pressed("jump") && is_on_floor(): velocity.y = jumpForce
+	
+func _physics_process(delta):
+	# Holding Physics Objects
 	handle_holding_objects()
-	
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity()* delta
-
-	# Handle jump.
+		# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-	
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("left", "right", "up", "down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-		#armature.rotation.y = lerp_angle(armature.rotation.y, atan2(-velocity.x,-velocity.z),LERP_VAL)
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+	# Gravity
+	if !is_on_floor(): velocity.y -= gravity * delta
+	if Input.is_action_just_pressed("jump") && is_on_floor(): velocity.y = jumpForce
+	
+	# Movement
+	velocity.x = lerp(velocity.x, moveDir.x * moveSpeed, acceleration * delta)
+	velocity.z = lerp(velocity.z, moveDir.z * moveSpeed, acceleration * delta)
+	
 	move_and_slide()
 	
-func set_held_object(body: RigidBody3D):
+func _unhandled_input(event):
+	# Camera Rotation
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * mouseSens.x * 0.01)
+		camera.rotate_x(-event.relative.y * mouseSens.y * 0.01)
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-90), deg_to_rad(90))
+		
+func set_held_object(body):
 	if body is RigidBody3D:
-		heldObject=body
-
+		heldObject = body
+	
 func drop_held_object():
-	heldObject=null
-
+	heldObject = null
+	
 func throw_held_object():
 	var obj = heldObject
 	drop_held_object()
-	obj.apply_central_impulse(-camera.global_transform.basis.z * throwForce * 10)
-		
+	obj.apply_central_impulse(-camera.global_basis.z * throwForce * 10)
+	
 func handle_holding_objects():
-	if Input.is_action_just_pressed("grab"):
-		if heldObject != null :throw_held_object()
+	# Throwing Objects
+	if Input.is_action_just_pressed("throw"):
+		if heldObject != null: throw_held_object()
 		
+	# Dropping Objects
 	if Input.is_action_just_pressed("interact"):
-		if heldObject !=null: drop_held_object()
-		elif InteractRay.is_colliding(): set_held_object(InteractRay.get_collider())
+		if heldObject != null: drop_held_object()
+		elif interactRay.is_colliding(): set_held_object(interactRay.get_collider())
 		
-	if heldObject !=null:
-		var targetpos= camera.global_transform.origin + (camera.global_basis*Vector3(0,0,-followDistance))
-		var objectPos= heldObject.global_transform.origin
-		heldObject.linear_velocity=(targetpos-objectPos)*followSpead
+	# Object Following
+	if heldObject != null:
+		var targetPos = camera.global_transform.origin + (camera.global_basis * Vector3(0, 0, -followDistance)) # 2.5 units in front of camera
+		var objectPos = heldObject.global_transform.origin # Held object position
+		heldObject.linear_velocity = (targetPos - objectPos) * followSpeed # Our desired position
 		
-		if heldObject.global_position.distance_to(camera.global_position)> maxDistanceFromCanere:
+		# Drop the object if it's too far away from the camera
+		if heldObject.global_position.distance_to(camera.global_position) > maxDistanceFromCamera:
 			drop_held_object()
 			
+		# Drop the object if the player is standing on it (must enable dropBelowPlayer and set a groundRay/RayCast3D below the player)
+		if dropBelowPlayer && groundRay.is_colliding():
+			if groundRay.get_collider() == heldObject: drop_held_object()
